95a6478c7b30af8f36e26e25196dfcdb9d233ae4
cristian98st <cris_st7@yahoo.com>
2019-01-11 22:33:07 +0200

    add Function

--- Makefile
+++ Makefile
@@ -7,3 +7,7 @@ clean:
 run:
 	kast test.asm
 	krun test.asm
+
+test:
+	kompile asm.k
+	krun test.asm
--- test.asm
+++ test.asm
@@ -1,3 +1,6 @@
-mov EAX, 2
-mov EBX, 7
-mov ECX, EAX
+mov EAX, 3
+mov EDX, 5
+mov EBX, 70
+mov EAX, EDX
+add EAX, 76
+mov EBX, 71
