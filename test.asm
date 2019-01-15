e47a21771f2cd84832b96d2d2275eb132d762156
cristian98st <cris_st7@yahoo.com>
2019-01-15 21:10:30 +0200

    Read added

--- test.asm
+++ test.asm
@@ -1,20 +1,13 @@
 mov EAX, 10
-mov EBX, 20
-mov ECX, 30
 mov EDX, 40
-jmp -hap
+mov EBX, read
+jmp -dest
 
 mov EAX, EDX
-inc EAX
-
--altceva
-mov EBX, 100
-inc EBX
 
 -dest
-mov ECX, 100
-inc ECX
-
--hap
 mov EDX, 100
 inc EDX
+
+-out
+inc ECX
